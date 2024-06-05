d76911a72011d415a810de785e74cccbf843d61a
muntasir-smm <munna112ru@gmail.com>
2024-06-06 00:43:42 +0600

    Adding area title

--- js/countries.js
+++ js/countries.js
@@ -4,18 +4,52 @@ const loadCountries = () => {
     .then((data) => displayCountries(data));
 };
 const displayCountries = (countries) => {
-  console.log(countries[0].flags);
+  console.log(countries[0]);
   const countriesHTML = countries.map((country) => getCountryHTML(country));
   //   console.log(countriesHTML[0]);
   const container = document.getElementById("countries");
   container.innerHTML = countriesHTML.join("");
 };
 
-const getCountryHTML = (country) => {
+// original
+
+// const getCountryHTML = (country) => {
+//   return `
+//     <div class="country">
+//     <h2>${country.name.common}</h2>
+//     <img src="${country.flags.png}">
+//     </div>
+//     `;
+// };
+
+// Option 1
+
+// const getCountryHTML = (country) => {
+//   const { name, flags } = country;
+
+//   return `
+//     <div class="country">
+//     <h2>${name.common}</h2>
+//     <img src="${flags.png}">
+//     </div>
+//     `;
+// };
+
+// Option 2
+
+const getCountryHTML = ({ name, flags, area, region }) => {
+  //   const { name, flags } = country;
+
   return `
     <div class="country">
-    <h2>${country.name.common}</h2>
-    <img src="${country.flags.png}">
+    <h2 title="${name.official}">${name.common}</h2>
+    <!-- <h2>${name.common}<span class="officialName"> (${name.official})</span></h2> -->
+    
+    <p> Area:${area}km<sup>2</sup> </p>
+    <p>Continent: ${region}</p>
+
+
+    <img src="${flags.png}">
     </div>
     `;
 };
